7403276c6ff09ff182faef5863dcf81a5f1bee87
David Gjennestad <davigjenn@gmail.com>
2024-01-09 13:42:11 -0700

    Finished Prep2, now to do the Stretch Challenge

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        char grade;
+        string score;
+        int userScore;
+
+        Console.WriteLine("Welcome to the Grading Declcaration!");
+
+        Console.Write("Plesae enter your score: ");
+        score = Console.ReadLine();
+        userScore = int.Parse(score);
+
+        Console.WriteLine();
+        
+        if (userScore >= 90)
+        {
+            grade = 'A';
+        }
+
+        else if (userScore >= 80)
+        {
+            grade = 'B';
+        }
+
+        else if (userScore >= 70)
+        {
+            grade = 'C';
+        }
+
+        else if (userScore >= 60)
+        {
+            grade = 'D';
+        }
+
+        else
+        {
+            grade = 'F';
+        }
+
+        if (userScore >= 70)
+        {
+            Console.WriteLine($"Congrats on passing you got {userScore}. Which is the letter {grade}");
+        }
+
+        else 
+        {
+            Console.WriteLine("You did not pass.");
+        }
     }
 }
