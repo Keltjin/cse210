4210d725f9c898288740e6ae9229ec98cb14c437
David Gjennestad <davigjenn@gmail.com>
2024-01-11 13:43:40 -0700

    Add comments to explain how it works.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,14 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Declare all my variables
         string guessNumberString;
         string repeat;
         int magicNumber;
         int guessNumber;
         int counter;
 
+        // Use a do-while loop to run the guessing game at least once.
         do
-        {
+        {   
+            // Creates a random number to guess between 1 and 10 and ask the user for the prompt.
             counter = 0;
 
             Random randomGenerator = new Random();
@@ -21,10 +24,13 @@ class Program
             guessNumberString = Console.ReadLine();
             guessNumber = int.Parse(guessNumberString);
 
+            // Checks to see if user guess the number or not.
             while (guessNumber != magicNumber)
             {
+                // Keeps track of attempts to guess the number.
                 counter += 1;
 
+                // Lets the user know if they are higher or lower and prompts for a retry.
                 if (guessNumber > magicNumber)
                 {
                     Console.WriteLine("Lower");
@@ -44,6 +50,7 @@ class Program
                 }
             }
 
+            // Displays the result and asks the user if they want to play again.
             Console.WriteLine($"Congrats! You guess the right number! It took you {counter} times.");
             
             Console.Write("Would you like to play again? ");
