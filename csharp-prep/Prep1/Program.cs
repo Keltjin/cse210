23ec3b750419197d92c5c9a0f97d586036620604
David Gjennestad <davigjenn@gmail.com>
2024-02-06 13:35:50 -0700

    Redid Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,16 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Ask the user for their first and last name and sotes them into variables.
+        // Declares the variables for the user names.
+        string firstName;
+        string lastName;
+
+        // Asks the user for their first and last name.
         Console.Write("What is your first name? ");
-        string firstName = Console.ReadLine();
+        firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string lastName = Console.ReadLine();
+        lastName = Console.ReadLine();
 
-        Console.WriteLine();
+        // Print out the user's first and last name.
+        Console.WriteLine($"You name is {lastName}, {firstName}, {lastName}.");
 
-        // Displays the variables to the console.
-        Console.Write($"My name is {lastName}, {firstName} {lastName}.");
     }
 }
