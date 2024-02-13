65bcd50f0ec21c8fe7f4b92a933b7702d9c5cf51
David Gjennestad <davigjenn@gmail.com>
2024-02-13 11:05:08 -0700

    Finish the program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,8 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
-
         Journal menu = new Journal();
 
         menu.DisplayMenu();        
