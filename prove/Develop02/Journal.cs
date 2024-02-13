5f9cba45f13eee1e540fa0fda3e91306e5bd5373
David Gjennestad <davigjenn@gmail.com>
2024-02-13 11:04:21 -0700

    Created methods in the journal.cs to run the program.

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -31,79 +31,98 @@ public class Journal
 
             choice = Console.ReadLine();
 
-            if(choice == "1")
+            switch (choice)
             {
-                DateTime theCurrentTime = DateTime.Now;
-                PromptGenerator prompt = new PromptGenerator();
+                case "1":
+                    WriteEntry();
+                    break;
 
-                string dateText = theCurrentTime.ToShortDateString();
-                string randomPrompt = prompt.DisplayPrompt();
-                Console.WriteLine(randomPrompt);
+                case "2":
+                    DisplayEntries();
+                    break;
 
-                entry = Console.ReadLine();
-                entires.Add(entry);
-                dates.Add(dateText);
-            }
+                case "3":
+                    LoadEntries();
+                    break;
 
-            else if (choice == "2")
-            {
-                for (int i = 0; i < entires.Count; i++)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine($"{dates[i]} - {entires[i]}");
-                }
-            }
-
-            else if (choice == "3")
-            {
-                Console.Write("Enter the name of the file to load: ");
-                string fileName = Console.ReadLine();
-
-                if (File.Exists(fileName))
-                {
-                    using (StreamReader file = new StreamReader(fileName))
-                    {
-                        string line;
-                        while ((line = file.ReadLine()) != null)
-                        {
-                            string[] parts = line.Split(" - ", 2); 
-                            if (parts.Length == 2)
-                            {
-                                dates.Add(parts[0]);
-                                entires.Add(parts[1]);
-                            }
-                        }
-                    }
-                    Console.WriteLine("File loaded succesfully.");
-
-                    Console.WriteLine("\nLoaded Entries:");
-                    for (int i = 0; i < entires.Count; i++)
-                    {
-                        Console.WriteLine($"{dates[i]} - {entires[i]}");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("File does not exist.");
-                }
-            }
-            
-
-            else if (choice == "4")
-            {
-                Console.Write("What is name of the txt document you are saving it as? ");
-                string fileName = Console.ReadLine();
-
-                using (StreamWriter outputFile = new StreamWriter(fileName))
-                {
-                    for (int i = 0; i < entires.Count; i++)
-                    {
-                        outputFile.WriteLine($"{dates[i]} - {entires[i]}");
-                    }
-                }
+                case "4":
+                    SaveEntries();
+                    break;  
             }
 
         } while (choice != "5");
        
+    }
+
+    public void WriteEntry()
+    {
+        DateTime theCurrentTime = DateTime.Now;
+        PromptGenerator prompt = new PromptGenerator();
+
+        string dateText = theCurrentTime.ToShortDateString();
+        string randomPrompt = prompt.DisplayPrompt();
+        Console.WriteLine(randomPrompt);
+
+        entry = Console.ReadLine();
+        entires.Add(entry);
+        dates.Add(dateText);
+    }
+
+    public void DisplayEntries()
+    {
+        for (int i = 0; i < entires.Count; i++)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{dates[i]} - {entires[i]}");
+        }
+    }
+
+    public void LoadEntries()
+    {
+        Console.Write("Enter the name of the file to load: ");
+        string fileName = Console.ReadLine();
+
+        if (File.Exists(fileName))
+        {
+            using (StreamReader file = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(" - ", 2); 
+                    if (parts.Length == 2)
+                    {
+                        dates.Add(parts[0]);
+                        entires.Add(parts[1]);
+                    }
+                }
+            }
+
+            Console.WriteLine("File loaded succesfully.");
+
+            Console.WriteLine("\nLoaded Entries:");
+            for (int i = 0; i < entires.Count; i++)
+            {
+                Console.WriteLine($"{dates[i]} - {entires[i]}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("File does not exist.");
+        }
+    }
+
+    public void SaveEntries()
+    {
+        Console.Write("What is name of the txt document you are saving it as? ");
+        string fileName = Console.ReadLine();
+
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            for (int i = 0; i < entires.Count; i++)
+            {
+                outputFile.WriteLine($"{dates[i]} - {entires[i]}");
+            }
+        }
     }
 }
